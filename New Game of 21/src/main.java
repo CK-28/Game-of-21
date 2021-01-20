e42849028ccff2c8f58e030f164e8f7cabeea83a
Crestena Khidhir <59154699+CK-28@users.noreply.github.com>
2021-01-20 03:06:45 -0500

    Add files via upload

---
+++ New Game of 21/src/main.java
@@ -0,0 +1,36 @@
+/*Name: Crestena Khidhir
+ *Date: 03/11/2020
+ *Purpose: Simulate a game of 21 where the user plays against the computer
+ */
+
+//Required java libraries
+import java.io.*;
+import java.util.*;
+
+public class Khidhir_codegameof21
+{
+    public static void main (String[] args)
+    {
+        //Welcome Message
+        System.out.println ("****************************************************************************************************");
+        System.out.println ("                                        Welcome To Game Of 21!                                      ");
+        System.out.println ("****************************************************************************************************");
+        System.out.println ("                                                Rules                                               ");
+        System.out.println ("             Win instantly by getting a total of 21 OR win by aquiring the highest total            ");
+        System.out.println ("                              If you exceed 21 you will lose right away                             ");
+        System.out.println ("                          Jack, Queen, King = 10     Ace = 1 or 11                                  ");
+        System.out.println ("                    To make a choice enter the corrosponding number and hit enter                   ");
+        System.out.println ("****************************************************************************************************");
+
+        //Variable Declaration
+        Game newGame = Game();
+
+        //Input and Processing and Output
+        
+
+        //Goodbye Message
+        System.out.println ("\n****************************************************************************************************");
+        System.out.println ("                                       Thank You For Playing!                                       ");
+        System.out.println ("****************************************************************************************************");
+    } // main method
+} // Khidhir_codegame21 class
