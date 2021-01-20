ddbe22566b58440a9a5611056a8599f44d50cfa3
Crestena Khidhir <59154699+CK-28@users.noreply.github.com>
2021-01-20 02:36:50 -0500

    Delete Hand.java

--- Hand.java
+++
@@ -1,9 +0,0 @@
-/*Name: Crestena Khidhir
- *Date: 03/11/2020
- *Purpose: Deals with all Hand object matters. Adds and reads the cards "held" in player's and computer's hands. Does not affect game flow
- */
-
-public class Hand
-{
-    
-}
